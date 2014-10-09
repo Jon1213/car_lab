959eddeb614a2112cbd65433ea0581b4d009653b
Jon1213 <jon1213@gmail.com>
2014-10-09 10:10:43 -0700

    prototypes are awesome!

--- spec/test.js
+++ spec/test.js
@@ -5,95 +5,127 @@ var Car = require('../src/car.js'),
 expect = require('chai').expect;
 
 describe('Car', function(){
+  var mobile;
+  var date = new Date("October 8, 2014 21:21:00");
 
   beforeEach(function(){
-    // create a new myCar object each time
+    mobile = new Car("Ford", "Corolla", 2014, "red");
   });
 
   describe('#year', function(){
     //think about using the new Date() and getFullYear functions
     it('should be the current year', function(){
-
+      expect(date.getFullYear()).to.eql(mobile.year);
     });
   });
 
   describe('#state', function(){
     it('should initially be off', function(){
-
+      expect(mobile.state).to.eql(false);
     });
   });
 
-  describe('#previousOwners', function(){
+  describe('#previous_owners', function(){
     it('should initially be empty', function(){
-
+      expect(mobile.previous_owners.length).to.equal(0);
     });
   });
 
-  describe('#curretOwner', function(){
+  describe('#current_owner', function(){
     it('should initially be manufacturer', function(){
-
+      expect(mobile.current_owner).to.eql("manufacturer");
     });
   });
 
   describe('#passengers', function(){
     it('should initially be empty', function(){
-
+      expect(mobile.passengers).to.eql([]);
     });
   });
 
   describe('#sale', function(){
-
-    it('should move currentOwner to previousOwners array', function(){
-
+    
+    it('should move current_owner to previous_owners array', function(){
+      mobile.sale("Howard");
+      expect(mobile.previous_owners).to.eql(["manufacturer"]);
     });
 
-    it('should update currentOwner with the new owner', function(){
-
+    it('should update current_owner with the new owner', function(){
+      mobile.sale("Howard");
+      expect(mobile.current_owner).to.eql("Howard");
     });
   });
 
   describe('#paint', function(){
     it('should update the color of myCar', function(){
-
+      mobile.paint("fuchsia");
+      expect(mobile.color).to.eql("fuchsia");
     });
   });
 
   describe('#start', function(){
     it('should update the state to on', function(){
-
+      mobile.start();
+      expect(mobile.state).to.eql(true);
     });
   });
 
   describe('#off', function(){
     it('should update the state to off', function(){
-
+      mobile.off();
+      expect(mobile.state).to.eql(false);
     });
   });
 
   describe('#park', function(){
     it('should make sure to only work when the car is off', function(){
+      mobile.off();
+      expect(mobile.park()).to.eql(true);
+    });
 
+  });
+
+  describe('#driveTo', function(){
+    it('should log a destination and return true', function(){
+      mobile.start();
+      expect(mobile.driveTo()).to.eql(true);
     });
 
   });
 
   describe('#pickUp', function(){
     it('should add the passenger to the passengers array if car is on', function(){
-
+      mobile.start();
+      mobile.pickUp("Sandy");
+      expect(mobile.passengers).to.eql(["Sandy"]);
     });
-
+    
     it('should not modify the passengers array if car is off', function(){
-
+      mobile.start();
+      mobile.pickUp("Sandy");
+      mobile.off();
+      mobile.pickUp("Roger");
+      expect(mobile.passengers).to.eql(["Sandy"]);
     });
   });
 
   describe('#dropOff', function(){
+    
     it('should remove passenger from the passengers array if car is on', function(){
-
+      mobile.start();
+      mobile.pickUp("Sherman");
+      mobile.dropOff("Sherman");
+      expect(mobile.passengers).to.eql([]);
     });
-
+    
     it('should leave passenger in the passengers array if car is off', function(){
-
+      mobile.start();
+      mobile.pickUp("Sherman");
+      mobile.dropOff("Sherman");
+      mobile.pickUp("Rick");
+      mobile.off();
+      mobile.dropOff("Rick");
+      expect(mobile.passengers).to.eql(["Rick"]);
     });
   });
 
--- src/car.js
+++ src/car.js
@@ -1,13 +1,65 @@
 function Car(make, model, year, color){
-
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.passengers = [];
 }
 
-Car.prototype.sale = function(newOwner){
+Car.prototype.state = false; //false = off. because booleans are nicer on the memory than dual-state strings
+Car.prototype.previous_owners = [];
+Car.prototype.current_owner = "manufacturer";
 
+Car.prototype.sale = function(newOwner){
+	Car.prototype.previous_owners.push(Car.prototype.current_owner);
+	Car.prototype.current_owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
+	this.color = newColor;
+};
 
+Car.prototype.park = function(){
+	if(Car.prototype.state === false){
+		console.log("parked!!!111");
+		return true;
+	}
+	return null;
+};
+
+Car.prototype.start = function(){
+	Car.prototype.state = true;
+};
+
+Car.prototype.off = function(){
+	Car.prototype.state = false;
+};
+
+Car.prototype.driveTo = function(destination){
+	if(Car.prototype.state === true){
+		console.log("I'ma drivin' to " + destination);
+		return true;
+	}
+	return null;
+};
+
+Car.prototype.pickUp = function(name){
+	if(Car.prototype.state === true){
+		this.passengers.push(name);
+		console.log("Pickin' " + name + " on up.");
+		return true;
+	}
+	return null;
+};
+
+Car.prototype.dropOff = function(name){
+	if( (Car.prototype.state === true) && (this.passengers.indexOf(name) !== -1) ){
+		var seat = this.passengers.indexOf(name);
+		this.passengers.splice(seat, 1);
+		console.log("Droppin' " + name + " on off.");
+		return true;
+	}
+	return null;
 };
 
 module.exports=Car;
